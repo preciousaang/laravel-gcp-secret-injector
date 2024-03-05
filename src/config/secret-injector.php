a46b85dce14eaf5a008b77f7e8bcb145a34a33b0
Precious Ibeagi <preciousaang@gmail.com>
2024-03-05 15:57:01 +0100

    wip

--- src/config/secret-injector.php
+++ src/config/secret-injector.php
@@ -5,8 +5,5 @@ return [
 
     'project_id' => env('GCP_PROJECT_ID'),
     'includedEnvs' => ['local'],
-    'secrets' => [
-        'APP_ENV' => 'APP_ENV:latest',
-        'DB_PASSWORD' => 'DB_PASSWORD:latest'
-    ]
+    'secrets' => []
 ];
